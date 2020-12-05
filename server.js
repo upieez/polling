a991cb685b1399a36eb9c2d9a10eaa0487c82eea
upieez <samuelhww@gmail.com>
2020-12-05 18:47:31 +0800

    deployed to cpanel

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const createError = require('http-errors');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,19 +12,27 @@ const io = require('socket.io')(server);
 
 const PORT = 8000 || process.env.PORT;
 
+/* ---------------------------------------------------------------------------
+      If you deploy this application to a web server,
+      it usually runs behind a reverse proxy like NGINX,
+      then your whole cookie system might not work because
+      you have to add app.set trust proxy 1.
+------------------------------------------------------------------------------*/
+app.set('trust proxy', 1);
+
 /**
  * Middleware
  */
 app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 /**
  * View Engine
  */
-app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
 
 server.listen(PORT, () =>
 	console.log(`~~~ You are now listening and enjoying to port: ${PORT} ~~~`)
